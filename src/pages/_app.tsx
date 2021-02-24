cdd4eeaa6f5b2a0b695f05c527a9ffc021e66c87
magaliais <gabriel.ashm@hotmail.com>
2021-02-24 11:45:14 -0300

    att interface

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+// Import do contexto (integração entre os components)
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengeContext'
+
 import '../styles/global.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
