7d3c5a0d2e83ca0565cebae96f75dff48574c675
magaliais <gabriel.ashm@hotmail.com>
2021-02-24 14:00:57 -0300

    correcting minor grammatical errors

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -45,10 +45,10 @@ export function ChallengeBox() {
                 </div>
             ) : (
                 <div className={ styles.challengeNotActive }>
-                    <strong>Finalize um ciclo para recever um desafio</strong>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
                     <p>
                         <img src="icons/level-up.svg" alt="Level Up"/>
-                        Avace de level completando desafios.
+                        Avance de level completando desafios.
                     </p>
                 </div>
             )}
